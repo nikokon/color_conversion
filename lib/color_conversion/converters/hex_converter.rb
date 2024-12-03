940e64b349fe0922008e5b3b7620ccc77f8b80e2
nikokon <martenblomberg@gmail.com>
2024-12-03 23:11:01 +0100

    add support for hex with alpha value

--- lib/color_conversion/converters/hex_converter.rb
+++ lib/color_conversion/converters/hex_converter.rb
@@ -3,8 +3,7 @@ module ColorConversion
 
     def self.matches?(color)
       return false unless color.kind_of?(String)
-
-      color.include?("#") && [4, 7].include?(color.length)
+      color.include?("#") && [4, 5, 7, 9].include?(color.length)
     end
 
     private
@@ -14,13 +13,22 @@ module ColorConversion
 
       {:r => hex[0,2].hex,
        :g => hex[2,2].hex,
-       :b => hex[4,2].hex, 
-       :a => 1.0}
+       :b => hex[4,2].hex,
+       :a => hex[6,2].hex / 255.0}
     end
 
     def normalize_hex(hex)
       hex = hex.gsub("#", "")
-      (hex.length == 3 ? hex[0,1]*2 + hex[1,1]*2 + hex[2,1]*2: hex).downcase
+      case hex.length
+      when 3
+        hex.split("").map { |character| character.to_s * 2 }.join + "ff"
+      when 4
+        hex.split("").map { |character| character.to_s * 2 }.join
+      when 6
+        (hex + "ff")
+      when 8
+        hex
+      end.downcase
     end
   end
 end
